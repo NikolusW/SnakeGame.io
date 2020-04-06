015a6b91305f1ba6333734121f59428129331371
Nikolusw <nik@ou.edu>
2020-04-05 20:00:40 -0500

    Added comments for each function. Fixed make coin function redundancy

--- sketch.js
+++ sketch.js
@@ -23,7 +23,9 @@ function draw() {
   }
   text('score: ' + score, 10, 10);
 }
-
+/**
+*Creates the grid that is used for the backgroup
+*/
 function makeGrid() {
   var i;
   fill(255)
@@ -35,7 +37,10 @@ function makeGrid() {
     }
   }
 }
-
+/**
+*This functions insures that when a button is pressed, the *snake only moves once.
+*This will likely havely to be changed as the final version *will require different movement.
+*/
 function moveSnake() {
   if ((keyIsPressed == true) && ((key == 's') || (key == 'S'))) {
     moveDown();
@@ -64,42 +69,53 @@ function makeGame(x, y) {
   fill(100, 255, 100)
   rect(50 * coinX, 50 * coinY, 50, 50)
 }
-
+/**
+*Moves snake up
+*/
 function moveUp() {
   if (posY >= 1) {
     posY = posY - 1;
   }
 }
-
+/**
+*Moves snake down
+*/
 function moveDown() {
   if (posY <= 8) {
     posY = posY + 1;
   }
 }
-
+/**
+*Moves snake left
+*/
 function moveLeft() {
   if (posX >= 1) {
     posX = posX - 1;
   }
 }
-
+/**
+*Moves snake right
+*/
 function moveRight() {
   if (posX <= 8) {
     posX = posX + 1;
   }
 }
-
+/**
+*This ensures that when the key is released, the user will be *able to move once again.
+*/
 function keyReleased() {
   console.log(t)
   t = false;
   console.log(t)
 }
-
+/**
+*Creates the coin, makes a random location
+*/
 function makeCoin() {
   fill(100, 244, 244)
   coinX = getRandomInt(10);
   coinY = getRandomInt(10);
-  rect(50 * coinX, 50 * coinY, 50, 50);
   coin = true;
 }
 
